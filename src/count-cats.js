5199765be7d54f54950abc0b3abf4c08f48a257e
Alex Karunny <karunny.alex@gmail.com>
2020-02-23 19:40:32 +0300

    feat: add task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let num = 0;
+  let str;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      str = /^\^\^$/.test(arr[i][j]);
+      if (str) {
+        num += 1;
+      }
+      
+    }
+  
+    
+  }
+  return num;
 };

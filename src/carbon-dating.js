c87042afd438c74d970e54731f44a5830da40890
Alex Karunny <karunny.alex@gmail.com>
2020-03-02 12:00:28 +0300

    feat: add task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(activity) {
+  
+  if (typeof(activity) != 'string' ) return false;
+
+  let sampleActivity = parseFloat(activity);
+
+  if (sampleActivity <= 0 || sampleActivity >= 15 || isNaN(sampleActivity)) return false;
+
+  let age = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+
+  return age;
 };

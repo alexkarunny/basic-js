039283b16a4237d28c8cd6ef1493b9b781a92d0e
Alex Karunny <karunny.alex@gmail.com>
2020-03-02 11:35:34 +0300

    feat: add task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+
+  if (!Array.isArray(arr)) return false;
+
+  let nameDreamTeam = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof(arr[i]) != 'string') continue;
+
+    for (let j = 0; j< arr[i].length; j++) {
+      if (arr[i][j] == ' ') continue;
+      
+      nameDreamTeam.push(arr[i][j]);
+
+      break;
+    }
+    
+  }
+
+  return nameDreamTeam.join("").toUpperCase().split("").sort().join("");
+
+}

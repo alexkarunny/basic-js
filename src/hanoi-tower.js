2e561eed0633e4d5922242a7578d70f37af22283
Alex Karunny <karunny.alex@gmail.com>
2020-03-03 19:33:03 +0300

    feat: add task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    
+    let hanoiO = {turns: 0 , seconds : 0};
+
+    hanoiO.turns = Math.pow(2, disksNumber) - 1;
+    hanoiO.seconds = hanoiO.turns / (turnsSpeed / 3600);
+
+    return hanoiO;
 }

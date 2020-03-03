fe11034ac0efcf8e6335e657aa3a0c8eeca8daf0
Alex Karunny <karunny.alex@gmail.com>
2020-03-04 02:29:50 +0300

    feat: add task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) throw new Error();
+    if (arr.length == 0) return arr;
+
+    let newarr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            i++;
+            continue;
+        } else if (arr[i] == '--double-prev' && i == 0) {
+            continue;
+        } else if (arr[i] == '--double-prev') {
+            newarr.push(arr[i - 1]);
+            continue;
+        } else if (arr[i] == '--discard-prev' && i == 0) {
+            continue;
+        } else if (arr[i] == '--discard-prev') {
+            newarr.splice(-1, 1);
+            continue;
+        } else if (arr[i] == '--double-next' && i == arr.length - 1) {
+            continue;
+        } else if (arr[i] == '--double-next') {
+            newarr.push(arr[i + 1]);
+            continue;
+        }
+        newarr.push(arr[i]);
+    }
+
+    return newarr;
 };
